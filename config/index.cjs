543053bacb1440edb943121acd5df766e913b025
sixseven <75666448+Sixsevenl@users.noreply.github.com>
2023-07-07 10:06:47 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -7,8 +7,11 @@
  */
 const USER_CONFIG = {
 
+  // 请将以下代码按照图示粘贴到对应位置
+  USE_PASSAGE: 'push-deer',
+
   // 使用微信测试号：公众号APP_ID
-  APP_ID: '',
+  APP_ID: 'PDU23873TlS0L5lqXg6EkEJdyYpxFvtHZJ0CiySHh',
 
   // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: '',
